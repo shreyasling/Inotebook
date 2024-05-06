988103958ffee15fdc8e321fe905eb9ae1d775d9
shreyasling <shreyasling007@gmail.com>
2024-05-06 18:45:54 +0530

    unique email

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,4 @@
-const mongoose =require('mongoose')
+const mongoose =require('mongoose');
 const mongoURI="mongodb://localhost:27017/inotebook"
 
 const connectToMongo =()=>{
--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ connectToMongo();
 
 
 const app = express()
-const port = 3000
+const port = 3000;
 app.use(express.json())
 //available routes
 app.use('/api/auth',require('./routes/auth'))
