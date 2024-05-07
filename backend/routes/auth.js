3e1393ed1fccb2c01a3309e9b7c04af3ed893c9f
shreyasling <shreyasling007@gmail.com>
2024-05-07 22:04:19 +0530

    used await function

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,16 +8,26 @@ const { body, validationResult } = require('express-validator');
 router.post('/',[body('email').isEmail(),
     body('name').isLength({ min: 3 })],
     body('password').isLength({ min: 5 }),
-        (req,res)=>{
+        async(req,res)=>{
             const errors = validationResult(req);
             if (!errors.isEmpty()) {
               return res.status(400).json({ errors: errors.array() });
             }
-            User.create({
+            //check whether duplicate email exist or not
+           let user= await User.findOne({email:req.body.email});
+           if(user){
+            return res.status(400).json({email:"Sorry user already exists"})
+           }
+           user = await User.create({
                 name: req.body.name,
                 password: req.body.password,
                 email:req.body.email,
-              }).then(user => res.json(user));
+              })
+              
+              // .then(user => res.json(user))
+              // .catch(err=>{console.log(err)
+              // res.json({error:'Please enter unique value for email',message :err.message})})
             // res.send(req.body)
-});
+            res.send({"nice":"nice"});
+})
 module.exports =router;
